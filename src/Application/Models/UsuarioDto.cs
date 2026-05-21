7cca711eb401da0f6e3332df665dc113c169cdec
AgusRey04 <reymundez37@gmail.com>
2026-05-21 18:37:42 -0300

    Creo e implemento DTO para Usuario

---
+++ src/Application/Models/UsuarioDto.cs
@@ -0,0 +1,34 @@
+using Domain.Entities;
+using Domain.Enums;
+
+namespace Application.Models;
+
+public class UsuarioDto
+{
+    public int Id { get; set; }
+    public string? Nombre { get; set; }
+    public string? Email { get; set; }
+    public Rol Rol { get; set; }
+    public DateTime FechaRegistro { get; set; }
+
+    public static UsuarioDto Create(Usuario usuario)
+    {
+        var dto = new UsuarioDto();
+        dto.Id = usuario.Id;
+        dto.Nombre = usuario.Nombre;
+        dto.Email = usuario.Email;
+        dto.Rol = usuario.Rol;
+        return dto;
+    }
+
+    public static List<UsuarioDto> CreateList(List<Usuario> usuarios)
+    {
+        var dtos = new List<UsuarioDto>();
+        foreach (var usuario in usuarios)
+        {
+            dtos.Add(Create(usuario));
+        }
+        return dtos;
+    }
+
+}
