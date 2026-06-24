0733bcbe420bb09b0528fc76ffc5c4804c30731f
AgusRey04 <reymundez37@gmail.com>
2026-06-23 23:44:43 -0300

    modified:   src/Domain/Entities/Notificacion.cs

--- src/Domain/Entities/Notificacion.cs
+++ src/Domain/Entities/Notificacion.cs
@@ -6,14 +6,13 @@ namespace Domain.Entities
     {
         public int Id { get; set; }
         public int UsuarioId { get; set; } // A quién va dirigida
-        public int? ViajeId { get; set; } // Opcional, si la notificación está relacionada con un viaje 
+                                           // Opcional, si la notificación está relacionada con un viaje 
         public string Mensaje { get; set; }
         public DateTime Fecha { get; set; }
         public bool Leida { get; set; }
 
         // Propiedad de navegación
-        public Usuario? Usuario { get; set; }
-        public Viaje? Viaje { get; set; } // Si la notificación está relacionada con un viaje
+        public Usuario? Usuario { get; set; }// Si la notificación está relacionada con un viaje
 
         public Notificacion(int usuarioId, string mensaje)
         {
